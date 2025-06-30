cdfec30192fd36d8ff583b8d4a222493c537e125
lyhottag13 <leonardo@promoevt.com>
2025-06-30 08:40:45 -0700

    Implemented Chart.js
    This is an extremely handy tool. I was going to create my own chart tool, but decided against it.

--- public/app.js
+++ public/app.js
@@ -14,8 +14,19 @@ async function select() {
     const data = await res.json();
     const ret = getProblemSteps(data);
     console.log(ret);
+    new Chart(document.getElementById('pareto'), {
+        type: 'bar',
+        data: {
+            labels: ret.map(step => step.step),
+            datasets: [
+                {
+                    label: 'Steps and Fail Rates.',
+                    data: ret.map(step => step.count)
+                }
+            ]
+        }
+    });
 }
-
 function createButton() {
     const newButton = document.createElement('button');
     newButton.innerText = 'Select';
